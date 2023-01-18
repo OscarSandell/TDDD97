45201e4690fdba969b4187b044ed630cffcc680a
Alfred Spjut Molin <alfsp550@student.liu.se>
2023-01-18 10:53:41 +0100

    started on step 3

--- Lab1/client.js
+++ Lab1/client.js
@@ -4,5 +4,26 @@ displayView = () => {
 
 window.onload = () => {
     // window.alert("Hello TDDD97!");
+    changeView("welcomeview");
 };
 
+
+
+changeView = (newView) => {
+    var htmlView = document.getElementById(newView);
+    var oldView = document.getElementById("view");
+
+    oldView.innerHTML = htmlView.innerHTML;
+};
+
+
+validateLogin = () => {
+    console.log("YOlo");
+    var username = document.getElementsByName("username")[0];
+    var password = document.getElementsByName("password")[0];
+    console.log(username.value);
+    console.log(password.value);
+    const pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    console.log(pattern.test(username));
+    
+}
